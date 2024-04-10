b884a3c073beeaf1c40864eab74b68df8a438d7d
Adam Jimenez <outlandico@gmail.com>
2024-04-09 23:20:42 -0700

    code challenge 4 passed

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -92,8 +92,17 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map(arr => {
+    return arr
+      .filter(num => typeof num === 'number' && num % 5 === 0)
+      .map(num => Math.pow(2, num));
+  });
 };
+
+// Example usage:
+console.log(divisibleByFiveTwoToThePower([[0, 2, 5, 4], [2, 4, 10], []]));
+// Output: [ [1, 32], [1024], [] ]
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
@@ -210,7 +219,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return numbers divisible by five, then raise two to the power of the resulting numbers', () => {
     expect(divisibleByFiveTwoToThePower([[10, 20, 5, 4], [5, 6, 7, 9], [1, 10, 3]])).toStrictEqual([[1024, 1048576, 32], [32], [1024]]);
   });
